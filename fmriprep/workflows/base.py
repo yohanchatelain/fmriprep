4eeeaff7f85bd09c221bc625dde5fa02dbfdc787
Ross Blair <rblair2@stanford.edu>
2016-03-17 14:59:48 -0700

    start of removing t1 nodes and updating base workflow constructor to call new t1w_preprocessing workflow

--- fmriprep/workflows/base.py
+++ fmriprep/workflows/base.py
@@ -11,13 +11,16 @@ Created on Wed Dec  2 17:35:40 2015
 import os
 import os.path as op
 import pkg_resources as pkgr
-from ..variables_preprocessing import data_dir, work_dir, plugin, plugin_args
+
 from nipype.pipeline import engine as pe
 from nipype.interfaces import utility as niu
 from nipype.interfaces.fsl import (Merge, MCFLIRT, BET, FAST, FLIRT, TOPUP, FUGUE, BinaryMaths,
                                    UnaryMaths, ApplyWarp, ConvertXFM, ConvertWarp, Split, MeanImage)
-from nipype.interfaces.ants import N4BiasFieldCorrection, Registration, BrainExtraction, ApplyTransforms
+from nipype.interfaces.ants import N4BiasFieldCorrection, Registration, ApplyTransforms
+from nipype.interfaces.ants.segmentation import BrainExtraction
 
+from .anatomical import t1w_preprocessing
+from variables_preprocessing import data_dir, work_dir, plugin, plugin_args
 
 def fmri_preprocess(name='fMRI_prep', settings=None, subject_list=None):
     """
@@ -36,6 +39,8 @@ def fmri_preprocess(name='fMRI_prep', settings=None, subject_list=None):
     if subject_list is None or not subject_list:
         raise RuntimeError('No subjects were specified')
 
+    t1w_preprocessing_workflow = t1w_preprocessing()
+    
     workflow = pe.Workflow(name=name)
 
     inputnode = pe.Node(niu.IdentityInterface(
@@ -62,23 +67,6 @@ def fmri_preprocess(name='fMRI_prep', settings=None, subject_list=None):
     #SBRef_skull_strip.inputs.brain_probability_mask = "/home/cmoodie/Oasis_MICCAI2012-Multi-Atlas-Challenge-Data/T_template0_BrainCerebellumProbabilityMask.nii.gz"
     #SBRef_skull_strip.inputs.extraction_registration_mask = "/home/cmoodie/Oasis_MICCAI2012-Multi-Atlas-Challenge-Data/T_template0_BrainCerebellumRegistrationMask.nii.gz"
 
-    # T1 Bias Field Correction
-    n4 = pe.Node(N4BiasFieldCorrection(dimension=3, bspline_fitting_distance=300,
-                                       shrink_factor=3), name="Bias_Field_Correction")
-
-    # Skull strip the T1 with ANTS Brain Extraction
-    T1_skull_strip = pe.Node(
-        BrainExtraction(dimension=3), name="antsreg_T1_Brain_Extraction")
-    T1_skull_strip.inputs.brain_template = settings[
-        'skull_strip'].get('brain_template', pkgr.resource_filename('fmriprep', 'data/brain_template.nii.gz'))
-    T1_skull_strip.inputs.brain_probability_mask = settings[
-        'skull_strip'].get('brain_probmask', pkgr.resource_filename('fmriprep', 'data/brain_probmask.nii.gz'))
-    T1_skull_strip.inputs.extraction_registration_mask = settings[
-        'skull_strip'].get('reg_mask', pkgr.resource_filename('fmriprep', 'data/reg_mask.nii.gz'))
-
-    # fast -o fast_test -N -v
-    # ../Preprocessing_test_workflow/_subject_id_S2529LVY1263171/Bias_Field_Correction/sub-S2529LVY1263171_run-1_T1w_corrected.nii.gz
-    T1_seg = pe.Node(FAST(no_bias=True), name="T1_Segmentation")
 
     # Affine transform of T1 segmentation into SBRref space
     flt_wmseg_sbref = pe.Node(FLIRT(dof=6, bins=640, cost_func='mutualinfo'),
@@ -226,39 +214,6 @@ def fmri_preprocess(name='fMRI_prep', settings=None, subject_list=None):
     bbr_sbref_2_T1.inputs.schedule = settings['fsl'].get(
         'flirt_bbr', op.join(os.getenv('FSLDIR'), 'etc/flirtsch/bbr.sch'))
 
-    # ANTs registration
-    antsreg = pe.Node(Registration(), name="T1_2_MNI_Registration")
-    antsreg.inputs.fixed_image = settings['fsl'].get('mni_template', op.join(
-        os.getenv('FSLDIR'), 'data/standard/MNI152_T1_2mm_brain.nii.gz'))
-    antsreg.inputs.metric = ['Mattes'] * 3 + [['Mattes', 'CC']]
-    antsreg.inputs.metric_weight = [1] * 3 + [[0.5, 0.5]]
-    antsreg.inputs.dimension = 3
-    antsreg.inputs.write_composite_transform = True
-    antsreg.inputs.radius_or_number_of_bins = [32] * 3 + [[32, 4]]
-    antsreg.inputs.shrink_factors = [[6, 4, 2]] + [[3, 2, 1]]*2 + [[4, 2, 1]]
-    antsreg.inputs.smoothing_sigmas = [[4, 2, 1]] * 3 + [[1, 0.5, 0]]
-    antsreg.inputs.sigma_units = ['vox'] * 4
-    antsreg.inputs.output_transform_prefix = "ANTS_T1_2_MNI"
-    antsreg.inputs.transforms = ['Translation', 'Rigid', 'Affine', 'SyN']
-    antsreg.inputs.transform_parameters = [
-        (0.1,), (0.1,), (0.1,), (0.2, 3.0, 0.0)]
-    antsreg.inputs.initial_moving_transform_com = True
-    antsreg.inputs.number_of_iterations = ([[10, 10, 10]]*3 + [[1, 5, 3]])
-    antsreg.inputs.convergence_threshold = [1.e-8] * 3 + [-0.01]
-    antsreg.inputs.convergence_window_size = [20] * 3 + [5]
-    antsreg.inputs.sampling_strategy = ['Regular'] * 3 + [[None, None]]
-    antsreg.inputs.sampling_percentage = [0.3] * 3 + [[None, None]]
-    antsreg.inputs.output_warped_image = True
-    antsreg.inputs.use_histogram_matching = [False] * 3 + [True]
-    antsreg.inputs.use_estimate_learning_rate_once = [True] * 4
-    #antsreg.inputs.interpolation = 'NearestNeighbor'
-
-    # Transforming parcels from stan
-    at = pe.Node(ApplyTransforms(dimension=3, interpolation='NearestNeighbor',
-                                 default_value=0), name="Apply_ANTS_transform_MNI_2_T1")
-    at.inputs.input_image = settings['connectivity'].get(
-        'parellation', pkgr.resource_filename('fmriprep', 'data/parcellation.nii.gz'))
-
     invt_mat = pe.Node(
         ConvertXFM(invert_xfm=True), name="EpiReg_Inverse_Transform")
 
@@ -274,9 +229,11 @@ def fmri_preprocess(name='fMRI_prep', settings=None, subject_list=None):
         (inputnode, motion_correct_SE_maps, [('sbref', 'ref_file')]),
         (inputnode, EPI_BET, [('epi', 'in_file')]),
         (inputnode, SBRef_BET, [('sbref', 'in_file')]),
-        (inputnode, n4, [('t1', 'input_image')]),
-        (n4, T1_skull_strip, [('output_image', 'anatomical_image')]),
-        (n4, T1_seg, [('output_image', 'in_files')]),
+        #(inputnode, n4, [('t1', 'input_image')]),
+        #(n4, T1_skull_strip, [('output_image', 'anatomical_image')]),
+        #(n4, T1_seg, [('output_image', 'in_files')]),
+        #(n4, antsreg, [('output_image', 'moving_image')]),
+        #(antsreg, at, [('inverse_composite_transform', 'transforms')]),
         (T1_seg, flt_wmseg_sbref, [('tissue_class_map', 'in_file')]),
         (inputnode, flt_wmseg_sbref, [('sbref', 'reference')]),
         #(inputnode, SBRef_skull_strip, [("sbref", "in_file")]),
@@ -354,8 +311,6 @@ def fmri_preprocess(name='fMRI_prep', settings=None, subject_list=None):
         (fugue_dilate, bbr_sbref_2_T1, [('fmap_out_file', 'fieldmap')]),
         (flt_sbref_2_T1, bbr_sbref_2_T1, [('out_matrix_file', 'in_matrix_file')]),
         (T1_seg, bbr_sbref_2_T1, [('tissue_class_map', 'wm_seg')]),
-        (n4, antsreg, [('output_image', 'moving_image')]),
-        (antsreg, at, [('inverse_composite_transform', 'transforms')]),
         (inputnode, at, [('t1', 'reference_image')]),
         (bbr_sbref_2_T1, invt_mat, [('out_matrix_file', 'in_file')]),
         (at, flt_parcels_2_sbref, [('output_image', 'in_file')]),
