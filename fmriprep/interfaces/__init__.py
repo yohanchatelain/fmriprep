02e4c3f8b5d85150cd70cfceedfc3399f64e79f3
oesteban <code@oscaresteban.es>
2016-08-17 16:03:55 -0700

    [FIX] A first integration step after grabbids PR
    - [x] Added T1w-preprocessing workflow. Fixes #38
    - [ ] Working on the fieldmap estimation

--- fmriprep/interfaces/__init__.py
+++ fmriprep/interfaces/__init__.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-from fmriprep.interfaces.bids import ReadSidecarJSON, DerivativesDataSink
+from fmriprep.interfaces.bids import ReadSidecarJSON, DerivativesDataSink, BIDSDataGrabber
 from fmriprep.interfaces.utils import FormatHMCParam
