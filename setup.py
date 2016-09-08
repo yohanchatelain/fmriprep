d70fe40db79b0b7a2fb912949303df512c1459e4
Ross Blair <rosswilsonblair@gmail.com>
2016-09-08 07:28:20 -0700

    update requirements.txt to include pybids, setup.py needed iports from fmriprep.info

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@
 import os
 import sys
 
+from fmriprep.info import (__version__, __author__, __copyright__, __credits__,
+    __license__, __maintainer__, __email__, __status__, __url__,
+    __packagename__, __description__, __longdesc__)
 
 def main():
     """ Install entry-point """
