4fe9933e1b7f030ec0c4c19e31369c5e63fd1025
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-12-02 20:46:05 -0800

    added missing labels

--- fmriprep/workflows/base.py
+++ fmriprep/workflows/base.py
@@ -186,7 +186,7 @@ def wf_ds005_type(subject_data, settings, name='fMRI_prep'):
         (epi_2_t1, confounds_wf, [('outputnode.mat_t1_to_epi', 'inputnode.t1_transform')]),
 
         (hmcwf, epi_mni_trans_wf, [('inputnode.epi', 'inputnode.epi')]),
-        (epi_2_t1, epi_mni_trans_wf, [('outputnode.mat_epi_to_t1', 'inputnode.mat_epi_to_t1')]),
+        (epi_2_t1, epi_mni_trans_wf, [('outputnode.itk_epi_to_t1', 'inputnode.itk_epi_to_t1')]),
         (hmcwf, epi_mni_trans_wf, [('outputnode.xforms', 'inputnode.hmc_xforms'),
                                    ('outputnode.epi_mask', 'inputnode.epi_mask')]),
         (t1w_pre, epi_mni_trans_wf, [('outputnode.t1_brain', 'inputnode.t1'),
