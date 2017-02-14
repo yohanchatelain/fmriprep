464f2383231f038aa3de85b5c7128e604bb4fac1
Christopher J. Markiewicz <markiewicz@stanford.edu>
2017-02-14 13:55:59 -0500

    Add BIDSFreeSurferDir interface

--- fmriprep/interfaces/__init__.py
+++ fmriprep/interfaces/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-from fmriprep.interfaces.bids import ReadSidecarJSON, DerivativesDataSink, BIDSDataGrabber
+from fmriprep.interfaces.bids import ReadSidecarJSON, DerivativesDataSink, \
+    BIDSDataGrabber, BIDSFreeSurferDir
 from fmriprep.interfaces.images import ImageDataSink
 from fmriprep.interfaces.utils import FormatHMCParam, IntraModalMerge
