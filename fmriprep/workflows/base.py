d8eae609e8b66fa80551d3fc1ce20171780c604b
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2017-04-02 08:49:24 -0700

    remove debug print

--- fmriprep/workflows/base.py
+++ fmriprep/workflows/base.py
@@ -229,7 +229,6 @@ def basic_wf(subject_data, settings, name='fMRI_prep'):
         name = os.path.split(bold_file)[-1].replace(".", "_").replace(" ", "").replace("-", "_")
 
         # For doc building purposes
-        print(bold_file)
         if bold_file == 'sub-testing_task-testing_acq-testing_bold.nii.gz':
             metadata = {"RepetitionTime": 2.0,
                         "SliceTiming": [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}
