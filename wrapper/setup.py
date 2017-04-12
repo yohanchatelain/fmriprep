6827f4ae11fcd336dcd4fe05112088a4e3154e0a
Christopher J. Markiewicz <markiewicz@stanford.edu>
2017-04-12 11:11:36 -0400

    DIST: Move wrapper script and copy setup.py

---
+++ wrapper/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: oesteban
+# @Date:   2015-11-19 16:44:27
+""" fmriprep setup script """
+
+
+def main():
+    """ Install entry-point """
+    from io import open
+    from os import path as op
+    from glob import glob
+    from inspect import getfile, currentframe
+    from setuptools import setup, find_packages
+
+    this_path = op.dirname(op.abspath(getfile(currentframe())))
+
+    # Python 3: use a locals dictionary
+    # http://stackoverflow.com/a/1463370/6820620
+    ldict = locals()
+    # Get version and release info, which is all stored in fmriprep/info.py
+    module_file = op.join(this_path, 'fmriprep', 'info.py')
+    with open(module_file) as infofile:
+        pythoncode = [line for line in infofile.readlines() if not line.strip().startswith('#')]
+        exec('\n'.join(pythoncode), globals(), ldict)
+
+    setup(
+        name=ldict['__packagename__'],
+        version=ldict['__version__'],
+        description=ldict['__description__'],
+        long_description=ldict['__longdesc__'],
+        author=ldict['__author__'],
+        author_email=ldict['__email__'],
+        maintainer=ldict['__maintainer__'],
+        maintainer_email=ldict['__email__'],
+        url=ldict['__url__'],
+        license=ldict['__license__'],
+        classifiers=ldict['CLASSIFIERS'],
+        download_url=ldict['DOWNLOAD_URL'],
+        # Dependencies handling
+        setup_requires=[],
+        install_requires=ldict['REQUIRES'],
+        tests_require=ldict['TESTS_REQUIRES'],
+        extras_require=ldict['EXTRA_REQUIRES'],
+        dependency_links=ldict['LINKS_REQUIRES'],
+        package_data={'fmriprep': ['data/*.json', 'viz/*.tpl', 'viz/*.json']},
+        entry_points={'console_scripts': ['fmriprep=fmriprep.cli.run:main',]},
+        packages=find_packages(),
+        zip_safe=False
+    )
+
+if __name__ == '__main__':
+    main()
