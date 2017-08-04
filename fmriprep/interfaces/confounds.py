faa59136cddcb480c047ba2557302f6126543671
oesteban <code@oscaresteban.es>
2017-08-04 09:53:36 -0700

    fix failing doctest in confounds

--- fmriprep/interfaces/confounds.py
+++ fmriprep/interfaces/confounds.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
@@ -105,7 +104,8 @@ def _gather_confounds(signals=None, dvars=None, fdisp=None,
     >>> confound_list
     ['Global signals', 'DVARS']
 
-    >>> pd.read_csv(out_file, sep='\t', index_col=None)  # doctest: +NORMALIZE_WHITESPACE
+    >>> pd.read_csv(out_file, sep='\s+', index_col=None,
+    ...             engine='python')  # doctest: +NORMALIZE_WHITESPACE
          a    b
     0  0.1  0.2
     >>> tmpdir.cleanup()
