e12226f14238c032c0ed28ccdac91a593b9c5d01
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-08-08 17:20:49 -0400

    TEST: Add skip reason

--- fmriprep/workflows/bold/tests/test_util.py
+++ fmriprep/workflows/bold/tests/test_util.py
@@ -19,7 +19,8 @@ def symmetric_overlap(img1, img2):
 
 
 @pytest.mark.skipif(not os.getenv('FMRIPREP_REGRESSION_SOURCE') or
-                    not os.getenv('FMRIPREP_REGRESSION_TARGETS'))
+                    not os.getenv('FMRIPREP_REGRESSION_TARGETS'),
+                    reason='FMRIPREP_REGRESSION_{SOURCE,TARGETS} env vars not set')
 @pytest.mark.parametrize('input_fname,expected_fname', [
     (os.path.join(os.getenv('FMRIPREP_REGRESSION_SOURCE', ''),
                   base_fname),
