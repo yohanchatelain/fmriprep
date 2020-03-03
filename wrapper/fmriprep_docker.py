6e9859a96e65fc4620aaa01df9437d345a07b049
mathiasg <mathiasg@stanford.edu>
2020-03-03 11:08:33 -0500

    maint: bump license year [skip ci]

--- wrapper/fmriprep_docker.py
+++ wrapper/fmriprep_docker.py
@@ -20,7 +20,7 @@ import re
 import subprocess
 
 __version__ = '99.99.99'
-__copyright__ = 'Copyright 2019, Center for Reproducible Neuroscience, Stanford University'
+__copyright__ = 'Copyright 2020, Center for Reproducible Neuroscience, Stanford University'
 __credits__ = ['Craig Moodie', 'Ross Blair', 'Oscar Esteban', 'Chris Gorgolewski',
                'Shoshana Berleant', 'Christopher J. Markiewicz', 'Russell A. Poldrack']
 __bugreports__ = 'https://github.com/poldracklab/fmriprep/issues'
