5510b53d774ab7915892031f9091dc6a24811fa5
Elizabeth DuPre <emd222@cornell.edu>
2020-08-12 17:03:11 -0400

    ref: assume identity xforms, rather than varying number

--- fmriprep/workflows/bold/registration.py
+++ fmriprep/workflows/bold/registration.py
@@ -328,19 +328,14 @@ def init_bold_t1_trans_wf(freesurfer, mem_gb, omp_nthreads, multiecho=False, use
     gen_final_ref = init_bold_reference_wf(omp_nthreads, pre_mask=True)
 
     # Merge transforms placing the head motion correction last
-    nforms = 2 + int(use_fieldwarp)
     merge_xforms = pe.Node(niu.Merge(nforms), name='merge_xforms',
                            run_without_submitting=True, mem_gb=DEFAULT_MEMORY_MIN_GB)
-    if use_fieldwarp:
-        workflow.connect([
-            (inputnode, merge_xforms, [
-                ('fieldwarp', 'in2')])
-        ])
 
     workflow.connect([
         # merge transforms
         (inputnode, merge_xforms, [
-            ('hmc_xforms', 'in%d' % nforms),
+            ('hmc_xforms' , 'in3'),
+            ('fieldwarp', 'in2'),
             ('itk_bold_to_t1', 'in1')]),
         (merge_xforms, bold_to_t1w_transform, [('out', 'transforms')]),
         (inputnode, bold_to_t1w_transform, [('bold_split', 'input_image')]),
