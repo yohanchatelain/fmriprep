f0631cb29f098eb6096d223cd1627d21485a34dc
Oscar Esteban <code@oscaresteban.es>
2020-09-03 15:32:39 -0700

    fix: drop ``std_dvars`` as their correlation with ``dvars`` is 1.0 always

--- fmriprep/workflows/bold/confounds.py
+++ fmriprep/workflows/bold/confounds.py
@@ -330,7 +330,7 @@ Frames that exceeded a threshold of {regressors_fd_th} mm FD or
         import pandas as pd
         return [
             col for col in pd.read_table(table, nrows=2).columns
-            if not col.startswith(("a_comp_cor_", "t_comp_cor_"))
+            if not col.startswith(("a_comp_cor_", "t_comp_cor_", "std_dvars"))
         ]
 
     workflow.connect([
