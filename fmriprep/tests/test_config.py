a6d5f8b8f5818b7aac366ec6e01fe0a5ca33af00
mathiasg <mathiasg@stanford.edu>
2021-04-21 19:07:42 -0400

    ENH: Basic test for seed options

--- fmriprep/tests/test_config.py
+++ fmriprep/tests/test_config.py
@@ -1,6 +1,7 @@
 """Check the configuration module and file."""
 from pathlib import Path
 from pkg_resources import resource_filename as pkgrf
+import pytest
 from toml import loads
 from niworkflows.utils.spaces import format_reference
 
@@ -51,3 +52,19 @@ def test_config_spaces():
         format_reference((s.fullname, s.spec))
         for s in spaces.references if s.standard and s.dim == 3
     ] == ['MNI152NLin2009cAsym']
+
+
+@pytest.mark.parametrize("master_seed,ants_seed,numpy_seed", [
+    (1, 17612, 8272), (100, 19094, 60232)
+])
+def test_prng_seed(master_seed, ants_seed, numpy_seed):
+    """Ensure seeds are properly tracked"""
+    import os
+
+    seeds = config.seeds
+    seeds.load({'_random_seed': master_seed})
+    seeds.init()
+    assert seeds.master == master_seed
+    assert seeds.ants == ants_seed
+    assert seeds.numpy == numpy_seed
+    assert os.getenv("ANTS_RANDOM_SEED") == str(ants_seed)
