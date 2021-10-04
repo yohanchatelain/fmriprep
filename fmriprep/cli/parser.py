dc378457446d3838f6cea829b3a56d013903ecdf
Christopher J. Markiewicz <markiewicz@stanford.edu>
2021-10-04 09:47:11 -0400

    STY: Use pkgutil to determine if sentry_sdk is importable

--- fmriprep/cli/parser.py
+++ fmriprep/cli/parser.py
@@ -635,9 +635,8 @@ def parse_args(args=None, namespace=None):
     config.from_dict(vars(opts))
 
     if not config.execution.notrack:
-        try:
-            import sentry_sdk
-        except ImportError:
+        import pkgutil
+        if pkgutil.find_loader("sentry_sdk") is None:
             config.execution.notrack = True
             config.loggers.cli.warning("Telemetry disabled because sentry_sdk is not installed.")
         else:
